4b18ee81f8a6ace3fba75bd208c6fbb49853c39f
Cristiane Martins <crisplus@gmail.com>
2024-01-02 21:06:44 -0300

    conceito de statefull para podermos adicionar usuários na memória da aplicação, refatorar nossas rotas de listagem e criação para que possamos salvar e listar usuários, cabeçalhos de requisições e respostas e como utilizar para definir o formato de dados que são trafegados pela aplicação.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
 import http from 'node:http'
 
 
+const users = []
+
 const server = http.createServer((req, res) => {
     const {method, url} = req
     
     if(method == 'GET' && url == '/users') {
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     
     if(method == 'POST' && url == '/users') {
+        users.push({
+            id: 1,
+            name: 'Lana del Rey',
+            email: 'lanadelrey@gmail.com',
+        })
+
         return res.end('Criação de usuáro')
     }
 
